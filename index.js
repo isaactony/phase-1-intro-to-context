4a56ecbe489025f7caca6444348e45237f770ec0
isaac <isaactonyloi@gmail.com>
2022-12-12 09:24:06 +0300

    Trial solutions

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
 // Your code here
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(newarray) {
+    return newarray.map( function(row) {
+        //returns an object
+        return  createEmployeeRecord(row);
+    })
+              
+    
+}
+
+function createTimeInEvent (stamp) {
+    let [date, hour] = stamp.split(' ')
+
+    this.timeInEvents.push(
+        {
+            type: "TimeIn",
+            hour: parseInt(hour),
+            date,
+}
+        )
+        return this
+}
+
+function hoursWorkedOnDate(daysWorked) {
+    let startOfEvent = this.timeInEvents.find((e) => {
+        return e.date == daysWorked;
+    })
+    let endOfEvent = this.timeOutEvents.find((x) => {
+        return x.date == daysWorked;
+    })
+    return (startOfEvent.hour - endOfEvent.hour)/100
+
+}
+
+
+function wagesEarnedOnDate(dateSought){
+    let rawWage = hoursWorkedOnDate.call(this, dateSought)
+        * this.payPerHour
+    return parseFloat(rawWage.toString())
+}
+
+
+let allWagesFor = function(){
+    let eligibleDates = this.timeInEvents.map(function(e){
+        return e.date
+    })
+
+    let payable = eligibleDates.reduce(function(memo, d){
+        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0)
+
+    return payable
+}
+
+let findEmployeeByFirstName = function(srcArray, firstName) {
+  return srcArray.find(function(rec){
+    return rec.firstName === firstName
+  })
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
